afd543f64fe39a806ec66c41b66d921f658442a9
onion <kosukekito38@gmail.com>
2023-09-30 09:59:30 +0900

    style: fix style

--- tests/testthat/test-partial_eval_call.R
+++ tests/testthat/test-partial_eval_call.R
@@ -1,6 +1,6 @@
 test_that("Can partially evaluate a call", {
   # Arrange
-  call <- expression(x+y)[[1]]
+  call <- expression(x + y)[[1]]
   values <- list(x = 1)
 
   # Act
@@ -8,12 +8,12 @@ test_that("Can partially evaluate a call", {
 
   # Assert
   expect_equal(class(evaluated_call), "call")
-  expect_equal(evaluated_call, expression(1+y)[[1]])
+  expect_equal(evaluated_call, expression(1 + y)[[1]])
 })
 
 test_that("Can fully evaluate a call", {
   # Arrange
-  call <- expression(x+y)[[1]]
+  call <- expression(x + y)[[1]]
   values <- list(x = 1, y = 2)
 
   # Act
@@ -21,12 +21,12 @@ test_that("Can fully evaluate a call", {
 
   # Assert
   expect_equal(class(evaluated_call), "call")
-  expect_equal(evaluated_call, expression(1+2)[[1]])
+  expect_equal(evaluated_call, expression(1 + 2)[[1]])
 })
 
 test_that("Can deal with empty values", {
   # Arrange
-  call <- expression(x+y)[[1]]
+  call <- expression(x + y)[[1]]
   values <- list()
 
   # Act
@@ -34,18 +34,18 @@ test_that("Can deal with empty values", {
 
   # Assert
   expect_equal(class(evaluated_call), "call")
-  expect_equal(evaluated_call, expression(x+y)[[1]])
+  expect_equal(evaluated_call, expression(x + y)[[1]])
 })
 
 test_that("Can deal with unknown name in values", {
   # Arrange
-  call <- expression(x+y)[[1]]
-  values <- list(z=3)
+  call <- expression(x + y)[[1]]
+  values <- list(z = 3)
 
   # Act
   evaluated_call <- partial_eval_call(call = call, values = values)
 
   # Assert
   expect_equal(class(evaluated_call), "call")
-  expect_equal(evaluated_call, expression(x+y)[[1]])
+  expect_equal(evaluated_call, expression(x + y)[[1]])
 })
