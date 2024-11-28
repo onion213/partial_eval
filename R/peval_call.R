6637a190cac1c7125b8cbe2c393dcea26dd70b9f
onion <kosukekito38@gmail.com>
2024-11-28 02:27:39 +0000

    initial commit

---
+++ R/peval_call.R
@@ -0,0 +1,18 @@
+#' Partially evaluate a call
+#'
+#' @param call A call to be evaluated
+#' @param values A list or an environment object
+#' consists of some variable names in given call and values for them.
+#'
+#' @return A call.
+#' Substitute given values to some variables according to `values`
+#'
+#' @export
+#'
+#' @examples
+#' call <- expression(x + y)[[1]]
+#' values <- list(x = 1)
+#' partial_eval_call(call = call, values = values)
+peval_call <- function(call, values) {
+  do.call("substitute", args = list(call, env = values))
+}
